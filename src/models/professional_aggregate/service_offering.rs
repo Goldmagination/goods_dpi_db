ec5ada637d0deffc51e8181abbec5e9deb45ea77
David Ermakov <david.int.maurer@gmail.com>
2023-12-29 00:25:16 +0100

    adjsuted class

--- src/models/professional_aggregate/service_offering.rs
+++ src/models/professional_aggregate/service_offering.rs
@@ -6,4 +6,5 @@ pub struct ServiceOffering {
     pub professional_profile_id: i32,
     pub subcategory_id: i32,
     pub price: f64, // Assuming `decimal` maps to `f64`
+    pub subcategory_name: String,
 }
