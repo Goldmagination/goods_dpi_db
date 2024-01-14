946fc6062853d2817edbbed98407f4067ba2d1aa
David Ermakov <david.int.maurer@gmail.com>
2024-01-14 15:55:50 +0100

    func tests

--- src/dal/user_db.rs
+++ src/dal/user_db.rs
@@ -24,3 +24,31 @@ pub fn save_user_to_database(conn: &mut PgConnection, user_name: &str, user_emai
 
     Ok(())
 }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//     use diesel::pg::PgConnection;
+//     use crate::db::establish_connection;
+
+//     #[test]
+//     fn test_get_user_by_email() {
+//         let connection = setup_test_db();
+
+//         // Your known_category_id should be defined here
+//         let test_user_data = "test@test.com"; 
+
+
+//         let result = match get_user_by_email(&mut connection, test_user_data.to_string()){
+//             Ok(user) => Ok(()),
+//             Err(_) => panic!()};
+
+//         assert!(result.is_ok());
+//     }
+
+//     // Function to setup a test database connection
+//     fn setup_test_db() -> PgConnection {
+//         let connection = establish_connection(); // Use your connection setup function here
+//         connection.test_transaction().unwrap()
+//     }
+// }
